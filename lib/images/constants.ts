a9e65a03d7ce3b148c5efe7822060dc97df47400
kyuveloper <chaneegyu@gmail.com>
2026-05-14 15:36:02 +0900

    feat(M3.2): browser-image-compression 설치 + 이미지 상수

---
+++ lib/images/constants.ts
@@ -0,0 +1,4 @@
+export const STORAGE_LIMIT_BYTES = 100 * 1024 * 1024; // 100MB
+export const COMPRESS_MAX_WIDTH = 1920;
+export const COMPRESS_QUALITY = 0.8;
+export const COMPRESS_FILE_TYPE = "image/webp";
